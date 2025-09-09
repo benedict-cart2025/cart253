e0fad15c374050d54cc403ffe3a5e9e6b157acec
benedict-cart2025 <bentz1401@gmail.com>
2025-09-09 14:14:19 -0400

    so much drawing in p5 lol

--- drawing-practice/js/script.js
+++ drawing-practice/js/script.js
@@ -1,24 +1,49 @@
 /**
- * Title of Project
- * Author Name
+ * * Some Random Game
+ * Benedict Ambayec
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * One of the first prototypes of a game, I'll make on JS.
+ * Might be scuffed but it's all good, we're all learning.
+ * 
  */
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * woah, a setup!
 */
 function setup() {
-
+         // e=mc squared
+    createCanvas(640, 640);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * this is kinda difficult
 */
 function draw() {
+    // gris
+ background(150, 150, 150);
 
+ // The main part of the record is red
+    push();
+    fill(255, 0, 0);
+    stroke(255, 255, 255);
+    ellipse(320, 320, 480, 480);
+    pop();
+
+    // The label on the record
+    push();
+    fill(255, 255, 255);
+    noStroke();
+    ellipse(320, 320, 140, 140);
+    pop();
+
+    // The hole in the record
+    push();
+    fill(150, 150, 150);
+    stroke(50, 50, 50);
+    ellipse(320, 320, 20, 20);
+    pop();
 }
+
