83aa8ff2cc7cf19f4de61f5eb7aaa3ce0e7f8156
benedict-cart2025 <bentz1401@gmail.com>
2025-12-02 12:53:42 -0500

    wow

---
+++ variation-game1/js/questions.js
@@ -0,0 +1,58 @@
+// Questions array
+const questions = [
+  {
+    numb: 1,
+    question: "Which anime was the most popular from 2000 to 2010?",
+    answer: "Avatar The Last Airbender",
+    options: [
+      "Naruto: Shippuden",
+      "Avatar The Last Airbender",
+      "Fullmetal Alchemist: Brotherhood",
+      "Death Note"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which 90's NY rapper had the most album sales?",
+    answer: "2pac",
+    options: [
+      "2pac",
+      "Biggie Smalls",
+      "Jay-Z",
+      "Nas"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which Old Testament figure is the most prolific in Christianity?",
+    answer: "Luke",
+    options: [
+      "Abraham",
+      "Moses",
+      "Luke",
+      "Elijah"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Which Playstation exclusive franchise is the most popular?",
+    answer: "Halo",
+    options: [
+      "God of War",
+      "The Last Of Us",
+      "Halo",
+      "Horizon Zero Dawn"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Which reptile species the most populous in the world?",
+    answer: "Armadillo",
+    options: [
+      "Alligator",
+      "Snake",
+      "Turtle",
+      "Armadillo"
+    ]
+  },
+];
---
+++ variation-games/variation-game2/js/questions.js
@@ -0,0 +1,58 @@
+// Questions array
+const questions = [
+  {
+    numb: 1,
+    question: "?emit lla fo mlif detamina gnissorg-tsehgih eht si hcihW",
+    answer: "gniK noiL ehT",
+    options: [
+      "gniK noiL ehT",
+      "nezorF",
+      "ratavA",
+      "yrotS yoT"
+    ]
+  },
+  {
+    numb: 2,
+    question: "?htraE no naeco tsegral eht si tahW",
+    answer: "naecO cificaP ",
+    options: [
+      "naecO citcrA ",
+      "naecO cificaP ",
+      "naecO citnaltA",
+      "naecO naidnI"
+    ]
+  },
+  {
+    numb: 3,
+    question: "?ytivitaler fo yroeht eht depoleved tsitneics suomaf hcihW",
+    answer: "nietsniE treblA ",
+    options: [
+      "gnikwaH nehpetS ",
+      "notweN caasI",
+      "nietsniE treblA ",
+      "ielilaG oelilaG"
+    ]
+  },
+  {
+    numb: 4,
+    question: "?napaJ fo rewolf lanoitan eht si tahW",
+    answer: "mossolB yrrehC",
+    options: [
+      "esoR",
+      "mossolB yrrehC",
+      "yliL",
+      "dihcrO"
+    ]
+  },
+  {
+    numb: 5,
+    question: "?retaW yB derevoC sI ecafruS s'htraE fO egatnecreP hcuM woH",
+    answer: "%17",
+    options: [
+      "%17",
+      "%19",
+      "%16",
+      "%15"
+    ]
+  },
+];
