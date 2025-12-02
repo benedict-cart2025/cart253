85a7adcc865fe627ffe47c0a2768bfe78fcbb1b5
benedict-cart2025 <bentz1401@gmail.com>
2025-12-02 14:14:24 -0500

    so much code

---
+++ true-variation-jam/variation-game3/js/questions.js
@@ -0,0 +1,48 @@
+// Questions array
+const questions = [
+  {
+    numb: 1,
+    question: "Human blood contains gold.",
+    answer: "True",
+    options: [
+      "True",
+      "False"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Ketchup was once used to cure diseases like diarrhoea and colds ",
+    answer: "True",
+    options: [
+      "True",
+      "False"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Monaco is the smallest country in the world",
+    answer: "False",
+    options: [
+     "True",
+      "False"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Venus is the hottest planet in the solar system",
+    answer: "True",
+    options: [
+     "True",
+      "False"
+    ]
+  },
+  {
+    numb: 5,
+    question: "'A' is the most common letter used in the English language",
+    answer: "False",
+    options: [
+     "True",
+      "False"
+    ]
+  },
+];
