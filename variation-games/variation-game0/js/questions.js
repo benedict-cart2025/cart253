c106e1108a3a1ace02204610086e8d95105c4de5
benedict-cart2025 <bentz1401@gmail.com>
2025-12-02 00:56:50 -0500

    code
    lol

---
+++ variation-games/variation-game0/js/questions.js
@@ -0,0 +1,58 @@
+// Questions array
+const questions = [
+  {
+    numb: 1,
+    question: "What writing script is this: ខ្ញុំស្រលាញ់ប្រទេសកម្ពុជា",
+    answer: "Khmer",
+    options: [
+      "Thai",
+      "Devanagari",
+      "Khmer",
+      "Burmese"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Which is the world's most populous country?",
+    answer: "India",
+    options: [
+      "China",
+      "United States",
+      "Indonesia",
+      "India"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Which is the 4th most populated city in the world? ?",
+    answer: "Dhaka, Bangladesh",
+    options: [
+      "Shanghai, China",
+      "Tokyo, Japan",
+      "Delhi, India",
+      "Dhaka, Bangladesh"
+    ]
+  },
+  {
+    numb: 4,
+    question: "What language is this: 私は日本が大好きです",
+    answer: "Japanese",
+    options: [
+      "Chinese",
+      "Japanese",
+      "Korean",
+      "Vietnamese"
+    ]
+  },
+  {
+    numb: 5,
+    question: "How many countries are there in the world?",
+    answer: "195",
+    options: [
+      "193",
+      "194",
+      "195",
+      "196"
+    ]
+  },
+];
